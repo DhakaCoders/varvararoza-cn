a21361c1b204d8720527ddf403853817afd3d551
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-04-12 02:18:17 +0600

    ahhhh

--- themes/varvara/artists-list-single.php
+++ themes/varvara/artists-list-single.php
@@ -46,4 +46,50 @@ get_header(); ?>
   </div>
 </section>
 
+<div class="related-products clearfix">
+  <div class="wide-title"><div class="line"></div>
+    <div class="title">Related Products</div>
+    <div class="line"></div>
+  </div>
+  <ul class="product-lists clearfix">
+    <?php 
+    $product_thumb = '';
+    while($query->have_posts()): $query->the_post(); 
+    global $product, $woocommerce, $post;
+    $thumb_id = get_post_thumbnail_id($product->get_id());
+    if(!empty($thumb_id)){
+        $product_thumb = cbv_get_image_tag($thumb_id, 'prodgrid');
+    }
+    ?>
+    <li class="product-list gallery-col">
+
+    <div class="product-grid-inr">
+    <div class="fl-product-grd-img"><a href="<?php echo get_permalink( $product->get_id() ); ?>" class="overlay-link"></a>
+    <?php echo $product_thumb; ?>
+    </div>
+    <div class="fl-product-grd-des">
+        <h5 class="fl-product-grd-title"><a href="<?php echo get_permalink( $product->get_id() ); ?>"><?php echo get_the_title(); ?></a></h5>
+        <div class="fl-taxo">
+          <span class="fl-taxo1">Oil on Canvas</span>
+          <span class="fl-taxo2">Artist name</span>
+        </div>
+    </div>
+    <div class="wc-enquire">
+      <?php
+       if ($product->get_price() <= 0){
+      ?>
+      <a href="#">Enquire</a>
+      <?php }else{ ?>
+        <div class="loop-price">
+              <strong class="price-label">Price:</strong><?php echo $product->get_price_html(); ?>
+            </div>
+      <?php } ?>
+    </div>
+     
+    </div>
+    </li>
+    <?php endwhile; ?>
+  </ul>
+</div>
+
 <?php get_footer(); ?>
