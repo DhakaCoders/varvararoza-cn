4f89881d99dd65c569cef5b4c591c79116954ee3
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-04-11 21:31:25 +0600

    ggggg

--- themes/varvara/art-advisory-article.php
+++ themes/varvara/art-advisory-article.php
@@ -19,6 +19,29 @@ get_header();?>
   </div>
 </section><!-- end of page-banner -->
 
+<section class="art-article-content-sec-wrp">
+	<div class="container-fluid">
+	  <div class="row">
+	  	<div class="col-sm-12">
+	  	  <div class="art-article-content-block">
+	  	  	<div class="art-article-dsc-wrp">
+	  	  	  <div class="art-article-fl-img" style="background:url(<?php echo THEME_URI;?>/img/art-article-fl-img.png);"></div>
+	  	  	  <div class="art-article-dsc">
+	  	  	  	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam elementum scelerisque velit, eu egestas sem. In ultricies rutrum blandit. Phasellus ut imperdiet ligula. Nullam semper massa quis porta porttitor. Proin consequat erat vestibulum, laoreet lectus ut, porta nulla. Suspendisse potenti. Nullam tincidunt dictum purus, et condimentum mi consequat non. Nullam nec pellentesque risus, eget semper turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras id eros viverra, tempus erat eu, sodales elit. Fusce vitae eros aliquam, elementum justo vitae, consectetur felis. Curabitur odio elit, tincidunt et dui nec, mattis ornare augue. Nam porttitor dolor aliquam metus auctor, sit amet fermentum risus ultrices. Morbi et metus eu ipsum ultrices porttitor.</p>
+	  	  	  	<p>Nam id hendrerit leo. Curabitur a molestie lectus. Suspendisse sagittis porttitor bibendum. Maecenas ultricies sagittis velit, nec luctus metus eleifend non. Ut bibendum iaculis porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin gravida fermentum placerat. Morbi eu viverra libero. Aliquam vulputate feugiat justo, in posuere sem tempor convallis. Phasellus ultrices imperdiet nisl, ac posuere sem placerat vel. Sed ipsum augue, vestibulum vel urna id, viverra malesuada nunc. </p>
+	  	  	  </div>
+	  	  	  <div class="art-article-gallery-wrp">
+	  	  	  	<div class="art-article-gallery-grid">
+	  	  	  	  <div class="art-article-gallery-img" style="background:url(<?php echo THEME_URI;?>/img/art-article-gallery-img-1.png);"></div>
+	  	  	  	</div>
+	  	  	  </div>
+	  	  	</div>
+	  	  </div>
+	  	</div>
+	  </div>
+	</div>
+</section>
+
 <section class="art-post-grid-sec-wrp">
 	<div class="container-fluid">
 	  <div class="row">
