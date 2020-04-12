fd5c426b1c8aaa1d518c221e38eb0e3ba6a3da4e
azizulsoftsw <azizul.softsw@gmail.com>
2020-04-12 10:40:49 +0600

    js

--- themes/varvara/functions.php
+++ themes/varvara/functions.php
@@ -32,7 +32,7 @@ Enqueue Scripts->>
 */
 function cbv_theme_scripts(){
     //include_once( DIR . '/enq-scripts/popper.php' );
-    include_once( DIR . '/enq-scripts/bootstrap.php' );
+    include_once( DIR . '/enq-scripts/selectric.php' );
 }
 
 add_action( 'wp_enqueue_scripts', 'cbv_theme_scripts');
