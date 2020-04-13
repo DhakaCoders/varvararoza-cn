9a2a04d7ae433708821f1127ff5b09483f1c0c16
azizulsoftsw <azizul.softsw@gmail.com>
2020-04-13 20:40:18 +0600

    footer

--- themes/varvara/header.php
+++ themes/varvara/header.php
@@ -49,13 +49,13 @@ gtag('config', 'UA-148365752-1');
                     <?php } ?>
 
                 </a>
-
-                <div class="hdr-cart">
-                    <a href="#">
-                        <img src="<?php bloginfo('template_directory'); ?>/img/cart-icon.svg">
-                        <span>12</span>
-                    </a>
-                </div>
+    <?php 
+        if(WC()->cart->get_cart_contents_count() > 0){
+            echo sprintf ( '<div class="hdr-cart">
+                    <a href="%s">
+                        <img src="'.THEME_URI.'/img/cart-icon.svg"><span class="cart-count">%d</span></a></div>',wc_get_cart_url(),  WC()->cart->get_cart_contents_count() );
+        } 
+    ?>
                 
                 <div class="masthead__close" data-aos="fade-down">
                     <div class="masthead__close__img">
